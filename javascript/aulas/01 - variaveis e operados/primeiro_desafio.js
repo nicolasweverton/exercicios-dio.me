95049560979ec72b9ab10a077bc7fccf3ba7e9ab
nicolaswvrtn <nicolaswvrtn@gmail.com>
2024-04-24 15:47:40 -0300

    Create aula.js

---
+++ javascript/aulas/01 - variaveis e operados/primeiro_desafio.js
@@ -0,0 +1,8 @@
+
+let precoCombustivel = 4.5;
+let kmPorLitro = 10;
+let distanciaEmKm = 1589;
+
+const viagemKm = distanciaEmKm / kmPorLitro * precoCombustivel;
+
+console.log(viagemKm.toFixed(0));
