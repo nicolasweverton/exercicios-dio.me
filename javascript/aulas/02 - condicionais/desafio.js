afd4f3aa1c504bd81888521dcd459ec8122056f7
nicolaswvrtn <nicolaswvrtn@gmail.com>
2024-04-24 22:26:50 -0300

    Update desafio.js

--- javascript/aulas/02 - condicionais/desafio.js
+++ javascript/aulas/02 - condicionais/desafio.js
@@ -1,4 +1,4 @@
-const precoEtanol = 5.79;
+/*const precoEtanol = 5.79;
 const precoGasolina = 6.66;
 const kmPorLitros = 10;
 const distanciaEmKm = 100;
@@ -12,4 +12,20 @@ if (tipoCombustivel === 'Etanol') {
 } else {
   const valorGasto = litrosConsumidos * precoGasolina;
   console.log(valorGasto.toFixed(2));
+}*/
+
+const precoEtanol = 5.79;
+const precoGasolina = 6.66;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+const tipoCombustivel = 'Gasolina';
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+if (tipoCombustivel === 'Etanol'){
+  const valorGasto = litrosConsumidos * precoEtanol;
+  console.log(valorGasto.toFixed(2));
+} else {
+  const valorGasto = litrosConsumidos * precoGasolina;
+  console.log(valorGasto.toFixed(2))
 }
